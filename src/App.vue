d9be635f845d3a3c4582d694776d9c1f90ab4f6d
안치숙 <songweddin@naver.com>
2025-07-02 20:28:59 +0900

    Fix: '만' 중복 수정

--- src/App.vue
+++ src/App.vue
@@ -76,7 +76,7 @@ const megaCrewData = ref([
             <tbody>
               <tr v-for="data in megaCrewData" :key="data.team">
                 <td>{{ data.team }}</td>
-                <td>{{ data.views }}만</td>
+                <td>{{ data.views }}</td>
                 <td>{{ data.actualLike.toLocaleString() }}</td>
                 <td>{{ data.estimatedLike.toLocaleString() }}</td>
                 <td>
